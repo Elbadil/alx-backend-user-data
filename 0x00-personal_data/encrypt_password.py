d02c0cfb7e3481b4c07e5b3d3b43631e6ce9bdf9
Elbadil <elbaliadil@gmail.com>
2024-02-08 14:50:22 +0100

    Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Defining a function hash_password"""
 import bcrypt
-from typing import ByteString
 
 
-def hash_password(password: str) -> ByteString:
+def hash_password(password: str) -> bytes:
     """returns a hashed password"""
     b_password = password.encode()
     hashed_pswd = bcrypt.hashpw(b_password, bcrypt.gensalt())
