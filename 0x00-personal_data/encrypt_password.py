d7e33c66946fd395d4649f1f320dd774380f997b
Elbadil <elbaliadil@gmail.com>
2024-02-08 14:54:27 +0100

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Defining a function hash_password"""
+"""Defining two function hash_password and is_valid"""
 import bcrypt
 
 
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     b_password = password.encode()
     hashed_pswd = bcrypt.hashpw(b_password, bcrypt.gensalt())
     return hashed_pswd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validates that the provided password matches
+    the hashed password"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
