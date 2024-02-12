313b0e7fccb6085dc62a9015ffb20bdde7336fd5
Elbadil <elbaliadil@gmail.com>
2024-02-12 18:32:21 +0100

    Basic auth Class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,7 +15,10 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-if os.getenv('AUTH_TYPE'):
+if os.getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
     auth = Auth()
 
 
