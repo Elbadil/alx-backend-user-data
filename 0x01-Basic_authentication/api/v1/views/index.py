2105b241415fc21397aef15417dfbe945cc6d14e
Elbadil <elbaliadil@gmail.com>
2024-02-12 15:03:32 +0100

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized_request() -> None:
+    """unauthorized endpoint that returns status code 401"""
+    abort(401)
