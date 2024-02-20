91c1f63d656472005cb438d515682d71256c2c0a
Elbadil <elbaliadil@gmail.com>
2024-02-20 13:04:45 +0100

    Fixing errors with the add_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Saves a user to the database"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
