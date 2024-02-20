488fb2e61595dc57709538169e6a711e1a5c3806
Elbadil <elbaliadil@gmail.com>
2024-02-20 13:28:52 +0100

    Fixing errors with the add_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -40,13 +40,13 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs: Dict[str, str]) -> User:
+    def find_user_by(self, **kwargs: Dict[str, Union[str, int]]) -> User:
         """Finding a user based on the keyword arguments provided"""
         for key in kwargs.keys():
             if not hasattr(User, key):
                 raise InvalidRequestError()
 
-        user = self._session.query(User).filter_by(**kwargs).one()
+        user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
             raise NoResultFound()
 
