2afc8e2dbfda62fc4dde09d81f09dccb9033f3f2
Elbadil <elbaliadil@gmail.com>
2024-02-20 13:35:25 +0100

    Adding type annotations to the flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def bienvenue():
+def bienvenue() -> str:
     """returns Bienvenue as Json message"""
     return jsonify({"message": "Bienvenue"})
 
